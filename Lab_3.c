90758933c55ebed82588dcaabfec6f3425a9c691
Ahmed228 <45819707+Ahmed228@users.noreply.github.com>
2018-12-26 20:26:28 +0700

    Update Lab_3.c

--- Lab_3.c
+++ Lab_3.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
 #include <string.h>
-
-void check(unsigned char word[], unsigned char str[],int ind) {
-	long len = strlen(word);
-    for(int i=0; i < len; i++){
-        printf("%d ",ind + i - len + 1);
-        if(word[i] != str[i])
-            break;
+#include <math.h>
+#include <locale.h>
+int hashf(const unsigned char sample[], int M){
+    int h=0,i=0,p=1;
+    for(;i<M;++i){
+        h+=(((int)(sample[i]))%3)*p;
+        p*=3;
+    }
+    return h;
+}
+int needfornewhashf(const char sample[])   {
+    double M = strlen(sample);
+    return (int) pow(3.0, M - 1);
+}
+int newhashf(int h,const unsigned char out,const unsigned char in,int p){
+    return ((h-((int)(out)%3))/3+((int)(in)%3)*p);
+}
+void rk(const unsigned char sample[], int M, const unsigned char text[], int N,int p,int hs,int count){
+    int ht=hashf(text,M),counttext=0,countsample=0;
+    for(;counttext<=N-M;++counttext){
+        countsample=0;
+        if (hs==ht) for(;countsample<M;++countsample){
+            printf("%d %c", count+countsample+counttext+1,' ');
+            if (sample[countsample]!=text[countsample+counttext]) break;
+        }
+        ht=newhashf(ht,text[counttext],text[counttext+M],p);
     }
 }
 
-long hash(unsigned char word[]){
-    int N = strlen(word);
-    long sum = 0, s = 1;
-    for(int i = 0; i < N; i++){
-        sum += (word[i]%3)*s;
-		s *= 3;
-    }
-    return sum;
-}
-
-int main(void) {
-    unsigned char str[17] = {0},pattern[17] = {0};
-    gets(pattern);
-    long hash_pat = hash(pattern), s = 1;
-    int count = 0,ind = 1;
-    long hash_cur = 0, len = strlen(pattern);
-    int k = 0,fsym = 0;
-    printf("%ld ",hash_pat);
-	for (int i = 0; i < len-1; i++)
-		s *= 3;
-    while ((k = getchar()) != EOF){
-        str[count++] = k;
-
-        if(ind == len){
-            hash_cur=hash(str);
-        }
-        else if (ind > len){
-            hash_cur = ((hash_cur - fsym%3)/3) + (k%3)*s;
-        }
-
-        if (count == len){
-            if(hash_cur == hash_pat){
-                check(pattern, str, ind);
-            }
-            fsym = str[0];
-            memmove(str, str+1, len-1);
-            count--;
-        }
-        ind++;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    char sample[18], text[1025];
+    int hs = 0,count=0;
+    gets(sample);
+    hs = hashf(sample, strlen(sample));
+    int p = needfornewhashf(sample);
+    printf("%d %c", hs, ' ');
+    while (fgets(text,1024,stdin)){
+        rk(sample, strlen(sample), text, strlen(text),p, hs,count);
+        count+=strlen(text);
     }
     return 0;
 }
