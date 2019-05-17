1deee4819b0a235ab113b8b3b3f79a43c2b00bc1
Ahmed228 <45819707+Ahmed228@users.noreply.github.com>
2019-05-17 16:31:05 +0700

    Create 8-0-kruskal.c

---
+++ 8-0-kruskal.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct graph {
+    int start;
+    int end;
+    int weight;
+};
+
+void check1(int vershina, int rebro,FILE *out) {
+    if ((vershina < 0) || (vershina > 5000)) {
+        fprintf(out, "bad number of vertices");
+        exit(0);
+    }
+    if ((rebro < 0) || (rebro > (vershina * (vershina + 1) / 2))) {
+        fprintf(out, "bad number of edges");
+        exit(0);
+    }
+    if (vershina == 0) {
+        fprintf(out, "no spanning tree");
+        exit(0);
+    }
+}
+
+void check_Rebro(struct graph *gr, int ver, int i,FILE *out) {
+    if (gr[i].start < 1 || gr[i].start > ver) {
+        fprintf(out, "bad vertex");
+        exit(0);
+    }
+    if (gr[i].end < 1 || gr[i].end > ver) {
+        fprintf(out, "bad vertex");
+        exit(0);
+    }
+    if (gr[i].weight < 0 || gr[i].weight > INT_MAX) {
+        fprintf(out, "bad length");
+        exit(0);
+    }
+}
+
+void Sort(struct graph *gr, int l, int r) {
+    int i = l, j = r;
+    int a = gr[(l + r) / 2].weight;
+    do {
+        while (gr[i].weight < a) i++;
+        while (gr[j].weight > a) j--;
+        if (i <= j) {
+            struct graph c = gr[i];
+            gr[i] = gr[j];
+            gr[j] = c;
+            j--;
+            i++;
+        }
+    } while (i < j);
+    if (l < j) Sort(gr, l, j);
+    if (i < r) Sort(gr, i, r);
+}
+
+void makeSet(int *status, int x){
+    status[x] = x;
+}
+
+int find(int *status, int x){
+    if (status[x] == x) return x;
+    return status[x] = find(status, status[x]);
+}
+
+void unit(int x, int y, int *rank, int *status){
+    x = find(status, x);
+    y = find(status, y);
+    if (rank[x] < rank[y]) status[x] = y;
+    else {
+        status[y] = x;
+        if (rank[x] == rank[y]) rank[x]++;
+    }
+}
+
+void crascal(int *status, struct graph *gr, int vershina, int rebro, int *rank, FILE *out) {
+    for (int i = 1; i <= vershina; i++) makeSet(status, i);
+    for (int i = 0; i < rebro; i++){
+        if (find(status, gr[i].start) != find(status, gr[i].end)) {
+            unit(gr[i].start, gr[i].end, rank, status);
+            fprintf(out, "%d %d\n", gr[i].start, gr[i].end);
+        }
+    }
+}
+
+int main() {
+    FILE *in = fopen("in.txt", "r");
+    FILE *out = fopen("out.txt", "w");
+    struct graph *Grrra = NULL;
+    int vershina = 0; int rebro = 0; int err = 0; int ha = 0; int letter = 0;
+    int *status = NULL; int *rank = NULL; int *full = NULL;
+    fscanf(in, "%d", &vershina);
+    fscanf(in, "%d", &rebro);
+    check1(vershina, rebro, out);
+    Grrra = malloc(rebro * sizeof(struct graph));
+    status = malloc((vershina + 1) * sizeof(int));
+    rank = malloc((vershina + 1) * sizeof(int));
+    full = malloc((vershina + 1) * sizeof(int));
+    if ((vershina == 1) && (rebro == 0)) exit(0);
+    while ((fscanf(in, "%d", &letter)) != EOF) {
+        switch (err) {
+            case 0:Grrra[ha].start = letter;
+                err++;
+                break;
+            case 1:Grrra[ha].end = letter;
+                err++;
+                break;
+            case 2:Grrra[ha].weight = letter;
+                err = 0;
+                break;
+        }
+        if (err == 0) {
+            check_Rebro(Grrra, vershina, ha,out);
+            full[Grrra[ha].end] = 1;
+            full[Grrra[ha].start] = 1;
+            ha++;
+        }
+    }
+    if (ha != rebro) {
+        fprintf(out, "bad number of lines");
+        exit(0);
+    }
+    for (int i = 1; i <= vershina; i++) {
+        if (full[i] != 1) {
+            fprintf(out, "no spanning tree");
+            exit(0);
+        }
+    }
+    Sort(Grrra, 0, rebro - 1);
+    crascal(status, Grrra, vershina, rebro, rank, out);
+    free(status);
+    free(rank);
+    free(full);
+    free(Grrra);
+    fclose(in);
+    fclose(out);
+    return 0;
+}
